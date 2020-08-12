4a59895ef687fac8613db09ce7ab31b2034e0e05
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-12 16:43:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 document.addEventListener("DOMContentLoaded", function(){
   let element = document.querySelector("p#text");
-  
+  element.innerHTML = "This is really cool!";
+
 });
 
 console.log("This console.log() fires when index.js laods - before DOMContentLoaded is triggered");
